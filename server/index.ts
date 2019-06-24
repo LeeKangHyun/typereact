fc22769c6ebd9a3df5e1b9a8272ee399a2ba0bce
이강현 <khlee92@station3.co.kr>
2019-06-24 18:58:31 +0900

    typescript and cra, express

---
+++ server/index.ts
@@ -0,0 +1,22 @@
+import * as express from 'express'
+import * as path from 'path'
+import * as morgan from 'morgan'
+import * as http from 'http'
+
+const app = express()
+app.set('port', 3001)
+
+app.use(express.static(path.join(__dirname, '..', 'build', 'static')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+
+app.all('/*', (req, res, next) => {
+  console.log('Reading the main route through http request, sending index.html')
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
+})
+
+const server = http.createServer(app)
+server.listen(app.get('port'), function() {
+  console.log('Express Listening on Port ' + app.get('port'))
+})
