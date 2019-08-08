d49e1ddd4825819ed1256522f0756422c1a3cb9d
kanghyunlee <rkdgusdl92@gmail.com>
2019-08-08 22:34:19 +0900

    tests 추가
    tdd tests 코드 추가

--- src/pages/Chart/__tests__/Chart.test.tsx
+++ src/pages/Chart/__tests__/Chart.test.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom'
 import { act } from 'react-dom/test-utils'
 
 import HalfPie from '../Component/HalfPie'
+import Bar from '../Component/Bar'
 
 let container: HTMLDivElement | null
 
@@ -16,8 +17,8 @@ afterEach(() => {
   container = null
 });
 
-describe('파이 테스트', () => {
-  it('차트 그리기', () => {
+describe('Chart 테스트', () => {
+  it('Pie 차트 그리기', () => {
     act(() => {
       ReactDOM.render(
         <div>
@@ -25,6 +26,46 @@ describe('파이 테스트', () => {
             thickness={50}
             data={[0.45, 0.27, 0.27]}
             colors={['#9a3dea', '#59cedb', '#484c9f']}
+          />
+        </div>,
+        container
+      )
+    })
+  })
+
+  it('Bar 차트 그리기', () => {
+    act(() => {
+      ReactDOM.render(
+        <div>
+          <Bar
+            data={[
+              {
+                "letter": "A",
+                "frequency": 0.08167,
+              },
+              {
+                "letter": "B",
+                "frequency": 0.01492,
+              },
+              {
+                "letter": "C",
+                "frequency": 0.02782,
+              },
+              {
+                "letter": "D",
+                "frequency": 0.04253,
+              },
+              {
+                "letter": "E",
+                "frequency": 0.12702,
+              },
+            ]}
+            margin={{
+              top: 10,
+              right: 30,
+              bottom: 30,
+              left: 40
+            }}
           />
         </div>,
         container
